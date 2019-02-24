51e74719282ad600f15c2b6c35426d4f448f9bb2
fidgetspinnerkid <35818373+fidgetspinnerkid@users.noreply.github.com>
2019-02-24 10:08:11 -0500

    works, but doesnt have subtraction

--- Answerer.py
+++ Answerer.py
@@ -1,5 +1,32 @@
-import selenium
+from selenium import webdriver
+from Solver import Solver
 
 class Answerer:
-    def __init__():
-        
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def answer_basic_math(number_of_times):
+        driver = webdriver.Firefox()
+        for i in range(number_of_times):
+            driver.get("http://freerice.com/#/basic-math-pre-algebra/16859")
+            assert "Basic Math" in driver.title
+
+            question = driver.find_element_by_xpath("//*[@id=\"question-title\"]/a/b")
+
+            choices = driver.find_elements_by_class_name("answer-item")
+
+            print("question: "+str(question.text))
+            print("choices:")
+
+            for choice in choices:
+                print(choice.text)
+
+            answer = Solver.detect(question.text)
+            print("answer: "+ str(answer))
+
+            for choice in choices:
+                if str(answer) == choice.text:
+                    choice.click()
+                    break
+        driver.close()
--- Solver.py
+++ Solver.py
@@ -1,18 +1,24 @@
 class Solver:
     def __init__(self):
         pass
+
+    @staticmethod
     def add(string):
         numbers = string.replace(" ","")
         myList = numbers.split('+')
         firstNum = int(myList[0])
         secondNum = int(myList[1])
         return firstNum + secondNum
+        
+    @staticmethod
     def mult(string):
         numbers = string.replace(" ","")
         myList = numbers.split('x')
         firstNum = int(myList[0])
         secondNum = int(myList[1])
         return firstNum * secondNum
+
+    @staticmethod
     def detect(string):
         if "+" in string:
             return Solver.add(string)
