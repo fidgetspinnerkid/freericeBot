6c943026a45ffabbce652ce0869a14c9fc024994
fidgetspinnerkid <35818373+fidgetspinnerkid@users.noreply.github.com>
2019-02-24 14:47:45 -0500

    capital now works well

--- Answerer.py
+++ Answerer.py
@@ -120,3 +120,35 @@ class Answerer:
                 except selenium.common.exceptions.StaleElementReferenceException:
                     choices = driver.find_elements_by_class_name("answer-item")
         driver.close()
+
+    def answer_capital(number_of_times):
+        driver = webdriver.Firefox()
+        driver.get("http://freerice.com/#/world-capitals/13632")
+        for i in range(number_of_times):
+            time.sleep(0.8)
+
+            assert "Capital" in driver.title
+            assert "content-area" in driver.page_source
+
+            question = WebDriverWait(driver, 10).until(find_question)
+
+            choices = WebDriverWait(driver, 10).until(find_choices)
+            print("DONE WITH WebDriverWait")
+
+            try:
+                answer = Solver.find_capital(question.text, [i.text for i in choices])
+                print("I KNEW IT!!!!!!!!!!!!!!!!!!!!")
+            except selenium.common.exceptions.StaleElementReferenceException:
+                choices = WebDriverWait(driver, 4).until(find_choices)
+                question = WebDriverWait(driver, 10).until(find_question)
+                continue
+            except ValueError:
+                answer = choices[0].text
+
+            for choice in choices:
+                try:
+                    if str(answer) == choice.text:
+                        choice.click()
+                except selenium.common.exceptions.StaleElementReferenceException:
+                    choices = driver.find_elements_by_class_name("answer-item")
+        driver.close()
