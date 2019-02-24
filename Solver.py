f6cf1d30a0ece575c04d1c5c06789234713183da
Michael <agentdonoso@gmail.com>
2019-02-24 09:35:04 -0500

    Created script to Add and Multiply

--- Solver.py
+++ Solver.py
@@ -1,3 +1,20 @@
 class Solver:
-    def __init__():
-        
+    def __init__(self):
+        pass
+    def add(string):
+        numbers = string.replace(" ","")
+        myList = numbers.split('+')
+        firstNum = int(myList[0])
+        secondNum = int(myList[1])
+        return firstNum + secondNum
+    def mult(string):
+        numbers = string.replace(" ","")
+        myList = numbers.split('x')
+        firstNum = int(myList[0])
+        secondNum = int(myList[1])
+        return firstNum * secondNum
+    def detect(string):
+        if "+" in string:
+            return Solver.add(string)
+        else:
+            return Solver.mult(string)
