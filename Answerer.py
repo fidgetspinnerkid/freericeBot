b8f95f4ad0fd66b2396f8949fab9c9b0b1dacbcf
fidgetspinnerkid <35818373+fidgetspinnerkid@users.noreply.github.com>
2019-02-24 14:40:34 -0500

    chemistry works now very well

--- Answerer.py
+++ Answerer.py
@@ -27,7 +27,7 @@ class Answerer:
         driver = webdriver.Firefox()
         driver.get("http://freerice.com/#/basic-math-pre-algebra/16859")
         for i in range(number_of_times):
-            time.sleep(1)
+            time.sleep(0.8)
 
             assert "Basic Math" in driver.title
             assert "content-area" in driver.page_source
@@ -62,7 +62,7 @@ class Answerer:
         driver.get("http://freerice.com/#/english-vocabulary/13442")
         for i in range(number_of_times):
 
-            time.sleep(1)
+            time.sleep(0.5)
 
             assert "English" in driver.title
             assert "content-area" in driver.page_source
@@ -88,4 +88,35 @@ class Answerer:
                 except selenium.common.exceptions.StaleElementReferenceException:
                     choices = driver.find_elements_by_class_name("answer-item")
 
+        driver.close()
+
+    def answer_chem(number_of_times):
+        driver = webdriver.Firefox()
+        driver.get("http://freerice.com/#/chemical-symbols-full-list/1079")
+        for i in range(number_of_times):
+            time.sleep(0.8)
+
+            assert "Chemical" in driver.title
+            assert "content-area" in driver.page_source
+            question = WebDriverWait(driver, 10).until(find_question)
+
+            choices = WebDriverWait(driver, 10).until(find_choices)
+            print("DONE WITH WebDriverWait")
+
+            try:
+                answer = Solver.find_symbol(question.text, [i.text for i in choices])
+                print("I KNEW IT!!!!!!!!!!!!!!!!!!!!")
+            except selenium.common.exceptions.StaleElementReferenceException:
+                choices = WebDriverWait(driver, 4).until(find_choices)
+                question = WebDriverWait(driver, 10).until(find_question)
+                continue
+            except ValueError:
+                answer = choices[0].text
+
+            for choice in choices:
+                try:
+                    if str(answer) == choice.text:
+                        choice.click()
+                except selenium.common.exceptions.StaleElementReferenceException:
+                    choices = driver.find_elements_by_class_name("answer-item")
         driver.close()
--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from Solver import Solver
 from Answerer import Answerer
 import selenium
-
+import time
 
 def find_choices(driver):
     element = driver.find_elements_by_class_name("answer-item")
@@ -20,48 +20,7 @@ def find_question(driver):
         return False
 
 def main():
-    # mySolver = Solver()
-    #
-    # driver = webdriver.Firefox()
-    # driver.get("http://freerice.com/#/basic-math-pre-algebra/16859")
-    # assert "Basic Math" in driver.title
-    #
-    # question = driver.find_element_by_xpath("//*[@id=\"question-title\"]/a/b")
-    #
-    # choices = driver.find_elements_by_class_name("answer-item")
-    #
-    # print("question: "+str(question.text))
-    # print("choices:")
-    #
-    # for choice in choices:
-    #     print(choice.text)
-    #
-    # answer = mySolver.detect(question.text)
-    # print("answer: "+ str(answer))
-    #
-    # for choice in choices:
-    #     if str(answer) == choice.text:
-    #         choice.click()
-    #         break
-    #
-    # assert "Basic Math" in driver.title
-    #
-    # question = driver.find_element_by_xpath("//*[@id=\"question-title\"]/a/b")
-    #
-    # choices = driver.find_elements_by_class_name("answer-item")
-    #
-    # print("question: "+str(question.text))
-    # print("choices:")
-    # for choice in choices:
-    #     print(choice.text)
-    #
-    # answer = mySolver.detect(question.text)
-    # print("answer: "+ str(answer))
-    #
-    # driver.close()
-
-    Answerer.answer_basic_math(9999)
-    Answerer.answer_english(9999)
+    Answerer.answer_chem(9999999999999999999)
 
 if __name__ == "__main__":
     main()
